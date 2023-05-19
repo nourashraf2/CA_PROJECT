bb3867fe79446ad3572b038dd977ffbd341e0136
Shampoo <kareemelkaderi@gmail.com>
2023-05-19 17:31:03 +0300

    .

--- src/CPU.java
+++ src/CPU.java
@@ -1,62 +1,91 @@
 import java.io.IOException;
+import java.util.Hashtable;
 
 public class CPU {
-  private  InstructionMemory instructionMemory;
-  private  DataMemory dataMemory;
-  private Word8[]  registerFile;
-  private SREG sreg;
-  private int pc;
+   private InstructionMemory instructionMemory;
+   private DataMemory dataMemory;
+   private Word8[] registerFile;
+   private SREG sreg;
+   private int pc;
+   private Hashtable pipeline;
 
    public CPU() throws CpuException {
-      this.instructionMemory=new InstructionMemory();
-      this.dataMemory=new DataMemory();
-      registerFile=new Word8[64];
-      sreg=new SREG();
-      pc=0;
+      this.instructionMemory = new InstructionMemory();
+      this.dataMemory = new DataMemory();
+      registerFile = new Word8[64];
+      sreg = new SREG();
+      pc = 0;
 
    }
 
-   public Word16 fetch(int currPC){
-      Word16 wr=this.instructionMemory.getBlock()[currPC];
+   public Word16 fetch(int currPC) {
+      Word16 wr = this.instructionMemory.getBlock()[currPC];
       return wr;
-
    }
-//   public Hashtable decode(String instruction){
-//      Hashtable hashtable=new Hashtable<>();
-//
-//
-//
-//
-//
-//   }
+
+   public Hashtable decode(String instruction) { // 16 bits
+      Hashtable<String, Object> hashtable = new Hashtable<>();
+
+      String opcodeString = instruction.substring(0, 4);
+      String r1String = instruction.substring(4, 10);
+      String r2String = instruction.substring(10);
 
 
-    //Execute
+      int opcode = Integer.parseInt(opcodeString, 2);
+      int r1Address = Integer.parseInt(r1String, 2);
+      
+      hashtable.put("opcode", opcode);
+      hashtable.put("r1", registerFile[r1Address]);
 
-    //run
-    //3 + ((n − 1) ∗ 1)
-    //for(int i=0;i<clockCycles;i++){
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    // }
+      if (opcode < 3 || (opcode >= 5 && opcode <= 7)) { // R type
+         int r2Address = Integer.parseInt(r2String, 2);
+         hashtable.put("r2", registerFile[r2Address]);
+      } else { // I Type
+         hashtable.put("r2", twosBinaryStringToInt(r2String, 6));
+      }
+      return hashtable;
+   }
+
+   public int twosBinaryStringToInt(String binString) { // 1100110
+      int numOfBits = binString.length();
+      int result = Integer.parseInt(binString.substring(1), 2);
+      if (binString.charAt(0) == '1') {
+         result -= Math.pow(2, numOfBits-1);
+      } 
+      return result;
+   }
+
+   public void execute() {
+      // Execute ALU operation
+      //
+   }
+
+   // Execute
+
+   public void run(int clockCycles) {
+      for (int i = 0; i < clockCycles; i++) {
+
+         fetch(pc);
+         decode("");
+         execute();
+         pc++;
+         // instruction from memory -> fetch
+         // update pipeline fetch -> decode, decode -> execute
+      }
+   }
+
    public static void main(String[] args) {
       try {
-         CPU cpu=new CPU();
+         CPU cpu = new CPU();
          cpu.instructionMemory.loadMemory("instructions.txt");
-//         System.out.println(cpu.fetch(cpu.pc));
-        cpu.instructionMemory.displayMemory();
+         // System.out.println(cpu.fetch(cpu.pc));
+         cpu.instructionMemory.displayMemory();
 
       } catch (CpuException e) {
          System.out.println(e.getMessage());
       } catch (IOException e) {
          System.out.println(e.getMessage());
       }
-
 
    }
 
--- src/InstructionMemory.java
+++ src/InstructionMemory.java
@@ -1,123 +1,135 @@
 import java.io.*;
 
 public class InstructionMemory {
-  private  Word16[] block;
+    private Word16[] block;
+
     public InstructionMemory() throws CpuException {
-        this.block=new Word16[1024];
-//        for(int i=0;i<1024;i++) {
-//            block[i]=0;
-//
-//        }
+        this.block = new Word16[1024];
+        // for(int i=0;i<1024;i++) {
+        // block[i]=0;
+        //
+        // }
     }
 
-    public void displayMemory(){
-        int counter=0;
-        for(Word16 a :this.block){
-            if(a!=null){
-            System.out.println("Block " +counter+" " + a.toString());
-            counter++;
-            }
-            else{
-                System.out.println("Block " + counter+ " "+"null");
+    public void displayMemory() {
+        int counter = 0;
+        for (Word16 a : this.block) {
+            if (a != null) {
+                System.out.println("Block " + counter + " " + a.toString());
+                counter++;
+            } else {
+                System.out.println("Block " + counter + " " + "null");
                 counter++;
             }
 
-
         }
 
-
     }
+
     public void loadMemory(String filepath) throws IOException, CpuException {
         File file = new File(filepath);
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        BufferedReader br=new BufferedReader(fileReader);
-       String line;
-       int counter=0;
-        while ((line=bufferedReader.readLine()) != null) {
+        BufferedReader br = new BufferedReader(fileReader);
+        String line;
+        int counter = 0;
+        while ((line = bufferedReader.readLine()) != null) {
 
-            String [] instruction =line.split(" ");
-            String binaryInstruction="";
-            switch (instruction[0]){
-                case ("ADD"): binaryInstruction="0000" + registerEncode(instruction[1]) + registerEncode(instruction[2]);break;
-                case ("SUB"): binaryInstruction="0001" + registerEncode(instruction[1]) + registerEncode(instruction[2]);break;
-                case ("MUL"): binaryInstruction="0010" + registerEncode(instruction[1]) + registerEncode(instruction[2]);break;
-                case ("LDI"):  binaryInstruction="0011" + registerEncode(instruction[1]) + intEncode(instruction[2]);break;
-                case("BEQZ"): binaryInstruction="0100" + registerEncode(instruction[1]) +intEncode(instruction[2]);break;
-                case ("AND"): binaryInstruction="0101" + registerEncode(instruction[1]) + registerEncode(instruction[2]);break;
-                case ("OR") : binaryInstruction= "0110" + registerEncode(instruction[1]) +registerEncode(instruction[2]); break;
-                case ("JR") : binaryInstruction="0111" + registerEncode(instruction[1]) +registerEncode(instruction[2]); break;
-                case("SLC") : binaryInstruction="1000" +registerEncode(instruction[1]) + intEncode(instruction[2]);break;
-                case("SRC") :binaryInstruction="1001" +registerEncode(instruction[1]) + intEncode(instruction[2]);break;
-                case ("LB") :binaryInstruction="1010" +registerEncode(instruction[1]) +intEncode(instruction[2]); break;
-                case ("SB") :binaryInstruction="1011" +registerEncode(instruction[1]) +intEncode(instruction[2]); break;
-                default:throw new CpuException();
-
-
-
+            String[] instruction = line.split(" ");
+            String binaryInstruction = "";
+            switch (instruction[0]) {
+                case ("ADD"):
+                    binaryInstruction = "0000" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("SUB"):
+                    binaryInstruction = "0001" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("MUL"):
+                    binaryInstruction = "0010" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("LDI"):
+                    binaryInstruction = "0011" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                case ("BEQZ"):
+                    binaryInstruction = "0100" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                case ("AND"):
+                    binaryInstruction = "0101" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("OR"):
+                    binaryInstruction = "0110" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("JR"):
+                    binaryInstruction = "0111" + registerEncode(instruction[1]) + registerEncode(instruction[2]);
+                    break;
+                case ("SLC"):
+                    binaryInstruction = "1000" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                case ("SRC"):
+                    binaryInstruction = "1001" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                case ("LB"):
+                    binaryInstruction = "1010" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                case ("SB"):
+                    binaryInstruction = "1011" + registerEncode(instruction[1]) + intEncode(instruction[2]);
+                    break;
+                default:
+                    throw new CpuException();
 
             }
-//            System.out.println(binaryInstruction);
+            // System.out.println(binaryInstruction);
 
-            block[counter]=new Word16(binaryInstruction);
-            counter ++;
+            block[counter] = new Word16(binaryInstruction);
+            counter++;
 
         }
 
-
     }
-    public static String intEncode(String str) throws CpuException {  // takes str number "2" convert it into binary number used for immediate values
-        int num=Integer.parseInt(str);
-        if(num < 0 && num >= -32)
-            return Integer.toBinaryString(num).substring(26,32);
-        else
-            if(num >=0 && num <= 31){
-                String binaryString = Integer.toBinaryString(num);
-                int numBits = 6; // number of bits to represent the binary number
-                String paddedBinaryString = String.format("%" + numBits + "s", binaryString).replace(' ', '0');
-                return paddedBinaryString;
-            }
-            else{
-                throw new CpuException("Invalid number number should be between -32 and 31 inclusive");
 
+    public static String intEncode(String str) throws CpuException { // takes str number "2" convert it into binary
+                                                                     // number used for immediate values
+        int num = Integer.parseInt(str);
+        if (num < 0 && num >= -32)
+            return Integer.toBinaryString(num).substring(26, 32);
+        else if (num >= 0 && num <= 31) {
+            String binaryString = Integer.toBinaryString(num);
+            int numBits = 6; // number of bits to represent the binary number
+            String paddedBinaryString = String.format("%" + numBits + "s", binaryString).replace(' ', '0');
+            return paddedBinaryString;
+        } else {
+            throw new CpuException("Invalid number number should be between -32 and 31 inclusive");
 
-            }
-
-
+        }
 
     }
 
+    public static String registerEncode(String str) { // takes register R1 and convert it into a binary 000001
 
-
-    public static String registerEncode(String str){   // takes register R1  and convert it into a binary 000001
-
-        if(str.length()==3){
-            String temp=str.charAt(1)+""+str.charAt(2);
-            int registerNum=Integer.parseInt(temp);
+        if (str.length() == 3) {
+            String temp = str.charAt(1) + "" + str.charAt(2);
+            int registerNum = Integer.parseInt(temp);
             return toBitsUnsigned(registerNum);
-        }
-        else{
-            String temp=str.charAt(1)+"";
-            int registerNum=Integer.parseInt(temp);
+        } else {
+            String temp = str.charAt(1) + "";
+            int registerNum = Integer.parseInt(temp);
             return toBitsUnsigned(registerNum);
 
         }
     }
 
-    public static String toBitsUnsigned(int num) {       // takes an int a convert it into binary
+    public static String toBitsUnsigned(int num) { // takes an int a convert it into binary
         StringBuilder sb = new StringBuilder();
         for (int i = 31; i >= 0; i--) {
             sb.append((num >>> i) & 1);
         }
-        return sb.toString().substring(26,32);    //
+        return sb.toString().substring(26, 32); //
     }
 
-
-    public static void main (String[]args) throws CpuException, IOException {
-      InstructionMemory instructionMemory=new InstructionMemory();
+    public static void main(String[] args) throws CpuException, IOException {
+        InstructionMemory instructionMemory = new InstructionMemory();
         instructionMemory.loadMemory("instructions.txt");
         instructionMemory.displayMemory();
-
 
     }
 
